4e5d3e3cfd708263af38b76ff2f795810e8285ba
Sajna B John <sajna.john@INTDEVCCS.IN>
2020-03-09 13:27:23 +0530

    unwanted code removed

--- src/app/shared/models/meassage.ts
+++ src/app/shared/models/meassage.ts
@@ -1,4 +1,3 @@
-import { Publication } from 'src/app/shared/models/publication';
 export interface Message {
     updatePublicationMsg    : "Publication updated successfully.";
     addPublicationMsg       : "Publication added successfully.";
